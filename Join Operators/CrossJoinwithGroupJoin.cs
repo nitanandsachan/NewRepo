de1a661289df283f3b7be73e1009e6ce28cebe7b
nitanandsachan <sachan2019@hotmail.com>
2017-04-13 14:10:16 +0530

    Update CrossJoinwithGroupJoin.cs

--- Join Operators/CrossJoinwithGroupJoin.cs
+++ Join Operators/CrossJoinwithGroupJoin.cs
@@ -1,1 +1,45 @@
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examples.Expressions.Eval.LINQ_Dynamic.Join_Operators
+{
+    public partial class CrossJoinwithGroupJoin : Form
+    {
+        public CrossJoinwithGroupJoin()
+        {
+            InitializeComponent();
+        }
+
+        private void uiCrossJoinwithGroupJoin_LINQ_Execute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiCrossJoinwithGroupJoin_LINQ_Dynamic_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiCrossJoinwithGroupJoin_LINQ_Click(object sender, EventArgs e)
+        {
+            string[] categories = new string[]{"Beverages","Condiments","Vegetables","Dairy Products","Seafood" };
+
+            var products = My.GetProductList();
+
+            var q = from c in categories   join p in products on c equals p.Category into ps   from p in ps   select new { Category = c, p.ProductName };
+
+            foreach (var v in q)
+            {
+                Console.WriteLine(v.ProductName + ": " + v.Category);
+            }
+        }
+    }
+}
